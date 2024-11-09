61d0cefea45904fdd9439ebeacb83a87a56cf066
jobayer-alam-24 <sheikhnayamul123@gmail.com>
2024-11-09 22:17:36 +0600

    working

--- Exercise/Exercise82.cs
+++ Exercise/Exercise82.cs
@@ -12,6 +12,7 @@ namespace Exercise
             
             Console.WriteLine($"Removed Vowel: {removedVowels}");
             /*I am NEW TO SHOW ON GIT PULL*/
+            // I have changed you
         }
     }
 }
